8b0bdd73396863044a6e60c4a579d75724e13a27
stuckata <stu.kirkov@gmail.com>
2014-07-21 17:07:05 +0300

    homework 02 solved

---
+++ powerfulCars/checkingBits.js
@@ -0,0 +1,19 @@
+﻿function bitChecker(value) {
+
+	var result = true;
+
+	value = value >> 3;
+	value = value & 1;
+
+	if (value == 1) {
+		result = true;
+		return result;
+	} else {
+		result = false;
+		return result;
+	}
+}
+
+console.log(bitChecker(333));
+console.log(bitChecker(425));
+console.log(bitChecker(2567564754));
---
+++ powerfulCars/cylinderVol.js
@@ -0,0 +1,10 @@
+﻿function calcCylinderVol(value) {
+	
+	var volume = Math.pow(value[0], 2) * value[1] * Math.PI;
+	volume = volume.toFixed(3);
+	return volume;
+}
+
+console.log(calcCylinderVol([2, 4]));
+console.log(calcCylinderVol([5, 8]));
+console.log(calcCylinderVol([12, 3]));
